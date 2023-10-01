10751db32fe8976f71308e1820e030e883c2cccc
Dmitriy Varganov <varganovda@gmail.com>
2023-10-02 00:59:33 +0300

    refactor(code): reverted the lazy component usage

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,23 +1,15 @@
-import React, { Suspense, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
-const Header = React.lazy(() => import("home/Header"));
+import Header from "home/Header";
 import Footer from "home/Footer";
 
-const App = () => {
-  const [showHeader, setShowHeader] = useState(false);
-  return (
-    <div className="text-3xl mx-auto max-w-6xl">
-      {showHeader && (
-          <Suspense fallback={<div>Loading</div>}>
-            <Header/>
-          </Suspense>
-      )}
-      <button className="text-3xl p-5" onClick={() => setShowHeader(true)}>Show the header</button>
-      <div className="my-10">PDP page content</div>
-      <Footer />
-    </div>
-  );
-}
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">PDP page content</div>
+    <Footer />
+  </div>
+);
 ReactDOM.render(<App />, document.getElementById("app"));
