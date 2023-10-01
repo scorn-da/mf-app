008ed657e6a18f2dcdd0504b304327ccdb40a65c
Dmitriy Varganov <varganovda@gmail.com>
2023-10-02 00:57:22 +0300

    feat(project): tested a dynamic adding of the component after the usage on the page

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
-import Header from "home/Header";
+const Header = React.lazy(() => import("home/Header"));
 import Footer from "home/Footer";
 
-const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <Header />
-    <div className="my-10">PDP page content</div>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [showHeader, setShowHeader] = useState(false);
+  return (
+    <div className="text-3xl mx-auto max-w-6xl">
+      {showHeader && (
+          <Suspense fallback={<div>Loading</div>}>
+            <Header/>
+          </Suspense>
+      )}
+      <button className="text-3xl p-5" onClick={() => setShowHeader(true)}>Show the header</button>
+      <div className="my-10">PDP page content</div>
+      <Footer />
+    </div>
+  );
+}
 ReactDOM.render(<App />, document.getElementById("app"));
