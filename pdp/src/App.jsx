936ef1b86c83b93317467bc842884b10a1f9f8ed
Dmitriy Varganov <varganovda@gmail.com>
2023-10-02 02:58:21 +0300

    feat(code, ui): used the react-router-dom package

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -3,20 +3,24 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-import SafeComponent from "./SafeComponent";
 import Header from "home/Header";
 import Footer from "home/Footer";
 import PdpContent from "./PDPContent";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <SafeComponent>
-      <Header/>
-    </SafeComponent>
-    <div className="my-10">
-      <PdpContent />
-    </div>
-    <Footer />
-  </div>
+  <Router>
+    <Routes>
+      <Route path="/products/:id" element={
+        <div className="text-3xl mx-auto max-w-6xl">
+          <Header/>
+          <div className="my-10">
+            <PdpContent />
+          </div>
+          <Footer />
+        </div>
+      } />
+    </Routes>
+  </Router>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
