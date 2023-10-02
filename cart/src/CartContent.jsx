48ac95c1214ff254bdfebabfca46f12999c66db0
Dmitriy Varganov <varganovda@gmail.com>
2023-10-02 04:12:32 +0300

    feat(code): added a jwt showing

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from "./Login";
+import { jwt } from "./cart";
 
 const CartContent = () => {
   const [ token, setToken ] = useState("");
+
+  useEffect(() => {
+    return jwt.subscribe((val) => setToken(val ?? ""));
+  }, []);
 
   return (
     <div>
