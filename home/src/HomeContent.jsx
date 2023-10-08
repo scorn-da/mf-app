11ccccf03ceb4fda3edb1ea6557cb32752bd1dd8
Dmitriy Varganov <varganovda@gmail.com>
2023-10-09 01:14:27 +0300

    feat(code, ui): finished the routing between pages

--- home/src/HomeContent.jsx
+++ home/src/HomeContent.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 
 import { getProducts, currency } from "./products";
 import { addToCart, useLoggedIn } from "cart/cart";
@@ -15,10 +16,12 @@ const HomeContent = () => {
     <ul className="grid grid-cols-3 gap-5">
       {products?.map((product) => (
         <li key={product.id}>
-          <img src={product.image} alt={product.name} />
+          <Link to={`/products/${product.id}`}>
+            <img src={product.image} alt={product.name} />
+          </Link>
           <span className="flex">
             <b className="flex-grow font-bold">
-              <a href={`/products/${product.id}`}>{product.name}</a>
+              <Link to={`/products/${product.id}`}>{product.name}</Link>
             </b>
             <i className="flex-end">{currency.format(product.price)}</i>
           </span>
